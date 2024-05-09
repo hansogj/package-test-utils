4c37d2a97330f93565f046bf2fcf9f2bfb58f888
Hans Ole Gjerdrum <hansogj@gmail.com>
2024-05-09 12:50:37 +0200

    npm bump deps

--- shared/src/index.js
+++ shared/src/index.js
@@ -5,10 +5,10 @@ const shared = {
     html: require('./test.suite').html,
     dependencies: require('./dependencies').dependencies,
     versions: [
-        '@hansogj/abonnement-js@v4.1.0',
-        '@hansogj/array.utils@v2.1.0',
-        '@hansogj/find-js@v6.1.0',
-        '@hansogj/maybe@v2.2.14',
+        '@hansogj/abonnement-js@v4.3.0',
+        '@hansogj/array.utils@v2.3.0',
+        '@hansogj/find-js@v6.3.0',
+        '@hansogj/maybe@v2.2.23',
     ]
 }
 
